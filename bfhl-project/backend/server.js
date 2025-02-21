68d8c15bd53bd3430156d79c4441d73c33a3ef94
green119 <goyal.harshit17@gmail.com>
2025-02-21 19:17:31 +0530

    cors link

--- bfhl-project/backend/server.js
+++ bfhl-project/backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
     origin: [
       'http://localhost:3000',
-      'https://your-frontend.vercel.app' // Add your frontend Vercel URL here
+      'https://bfhl-project-harshit.vercel.app/' // Add your frontend Vercel URL here
     ],
     methods: ['GET', 'POST']
   }));
